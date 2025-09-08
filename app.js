1549d9aeb8f4ae76eaa0cac4715273f0c5744978
simisa <sdnpisanganbaru09pagi@gmail.com>
2025-09-08 23:05:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js")
+    navigator.serviceWorker.register("/service-worker.js")
       .then(reg => console.log("SW registered:", reg))
       .catch(err => console.error("SW registration failed:", err));
   });
